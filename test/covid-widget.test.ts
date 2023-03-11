ffb97246fd4f3bec3c9fbdec6a61c7d6481bd95b
Damir Nurgaliev <damir.nurgaliev@flatstack.com>
2023-03-11 23:05:56 +0100

    Initial commit

---
+++ test/covid-widget.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CovidWidget from '../lib/covid-widget-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/covid-widget-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CovidWidget.CovidWidgetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
